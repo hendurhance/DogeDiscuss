a299a86eb47a12109150207e7e7e50e10bb5c0b6
hendurhance <afriviral@gmail.com>
2021-11-29 18:28:34 +0100

    cross-origin fic

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <link rel="stylesheet" href="/css/app.css">
     <title>DogeDiscuss</title>
 </head>
 
@@ -18,7 +18,7 @@
         </v-app>
     </div>
 
-    <script src="{{asset('js/app.js')}}"></script>
+    <script src="/js/app.js"></script>
 </body>
 
 </html>
